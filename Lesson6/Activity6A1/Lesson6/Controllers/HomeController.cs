950bc3229515706d3a4a7a40071918336a2009ec
Onur Gumus <onur.gumus@emaratech.ae>
2017-10-17 16:43:27 +0400

    Initial commit.

---
+++ Lesson6/Activity6A1/Lesson6/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Lesson6.Controllers
+{
+  public class HomeController : Controller 
+    { 
+        // GET: /<controller>/ 
+        public IActionResult Index() 
+        { 
+            return Content("Index action method"); 
+        } 
+ 
+        public IActionResult Index2() 
+        { 
+            return Content("Index2 action method"); 
+        } 
+    } 
+
+
+}
---
+++ Lesson7/MVCEF/TagHelpers/ValidationClassTagHelper.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Linq;
+
+namespace MVCEF.TagHelpers
+{
+    [HtmlTargetElement(input, Attributes = ValidationAttributeName)]
+    public class ValidationClassTagHelper : TagHelper
+    {
+        private const string ValidationAttributeName = "bootstrap-validation";
+        private const string input = "input";
+        private const string aspFor = "asp-for";
+        private const string isInvalid = "is-invalid";
+        private const string isValid = "is-valid";
+
+
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+            ViewContext.ViewData.ModelState.TryGetValue(GetTargetName(), out var entry);
+            switch (entry)
+            {
+                case null: return;
+                case var _ when entry.Errors.Any():
+                    SetClass(isInvalid);
+                    return;
+                case var _:
+                    SetClass(isValid);
+                    break;
+            }
+
+
+            void SetClass(string cssClass)
+            {
+
+                var tagBuilder = new TagBuilder(input);
+                tagBuilder.AddCssClass(cssClass);
+                output.MergeAttributes(tagBuilder);
+            }
+
+            string GetTargetName() =>
+                ((ModelExpression)context.AllAttributes[aspFor].Value).Name;
+
+        }
+    }
+}
